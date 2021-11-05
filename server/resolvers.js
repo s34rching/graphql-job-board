92c6354f20c80b59fb7c2030f01920eb8939c0e8
s34rching <ivan.katsenya@gmail.com>
2021-11-05 17:24:35 +0300

    Create association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,11 @@
+const db = require('./db');
+
 const Query = {
-    greeting: () => 'Hello World!',
+    jobs: () => db.jobs.list(),
 }
 
-module.exports = { Query };
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+
+module.exports = { Query, Job };
